3a6bb8389a9857246f82afbb57a7818599e35597
Ashutosh Sharma <ashutoshhsharma2005@gmail.com>
2024-09-29 16:07:49 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,515 @@
+var arr0 = [
+  {
+    songName: "Bahane",
+    url: "./songs/Bollywood Tadka/b1.mp3",
+  },
+  {
+    songName: "Pagal",
+    url: "./songs/Bollywood Tadka/b2.mp3",
+  },
+  {
+    songName: "Aadat Lagi Hai",
+    url: "./songs/Bollywood Tadka/b3.mp3",
+  },
+  {
+    songName: "Coffee With Byah",
+    url: "./songs/Bollywood Tadka/b4.mp3",
+  },
+  {
+    songName: "Dhol Siphariya",
+    url: "./songs/Bollywood Tadka/b5.mp3",
+  },
+  {
+    songName: "Dil Todya",
+    url: "./songs/Bollywood Tadka/b6.mp3",
+  },
+  {
+    songName: "Doriye",
+    url: "./songs/Bollywood Tadka/b7.mp3",
+  },
+  {
+    songName: "Dost Yaad Aate Hai",
+    url: "./songs/Bollywood Tadka/b8.mp3",
+  },
+  {
+    songName: "Ek Baar Dekh Lijiye",
+    url: "./songs/Bollywood Tadka/b9.mp3",
+  },
+  {
+    songName: "Guzarne Do",
+    url: "./songs/Bollywood Tadka/b10.mp3",
+  },
+  {
+    songName: "Kho Jaana",
+    url: "./songs/Bollywood Tadka/b11.mp3",
+  },
+  {
+    songName: "Khuda Ke Bande",
+    url: "./songs/Bollywood Tadka/b12.mp3",
+  },
+  {
+    songName: "Main Aawara hun",
+    url: "./songs/Bollywood Tadka/b13.mp3",
+  },
+  {
+    songName: "Piya",
+    url: "./songs/Bollywood Tadka/b14.mp3",
+  },
+  {
+    songName: "Sabar",
+    url: "./songs/Bollywood Tadka/b15.mp3",
+  },
+  {
+    songName: "Sadqay",
+    url: "./songs/Bollywood Tadka/b16.mp3",
+  },
+  {
+    songName: "Same",
+    url: "./songs/Bollywood Tadka/b17.mp3",
+  },
+  {
+    songName: "Senorita",
+    url: "./songs/Bollywood Tadka/b18.mp3",
+  },
+  {
+    songName: "Zaalima",
+    url: "./songs/Bollywood Tadka/b19.mp3",
+  },
+  {
+    songName: "ZID",
+    url: "./songs/Bollywood Tadka/b20.mp3",
+  },
+];
+var arr1 = [
+  {
+    songName: "Heer",
+    url: "./songs/Punjabi Tadka/p1.mp3",
+  },
+  {
+    songName: "kashmir",
+    url: "./songs/Punjabi Tadka/p2.mp3",
+  },
+  {
+    songName: "That Girl",
+    url: "./songs/Punjabi Tadka/p3.mp3",
+  },
+  {
+    songName: "Arsa",
+    url: "./songs/Punjabi Tadka/p4.mp3",
+  },
+  {
+    songName: "Destiny",
+    url: "./songs/Punjabi Tadka/p5.mp3",
+  },
+  {
+    songName: "Goat Shit",
+    url: "./songs/Punjabi Tadka/p6.mp3",
+  },
+  {
+    songName: "GXCCI",
+    url: "./songs/Punjabi Tadka/p7.mp3",
+  },
+  {
+    songName: "havaa",
+    url: "./songs/Punjabi Tadka/p8.mp3",
+  },
+  {
+    songName: "House Of Lies",
+    url: "./songs/Punjabi Tadka/p9.mp3",
+  },
+  {
+    songName: "Inipu",
+    url: "./songs/Punjabi Tadka/p10.mp3",
+  },
+  {
+    songName: "Jagga Jatt",
+    url: "./songs/Punjabi Tadka/p11.mp3",
+  },
+  {
+    songName: "Je Jatt Vigad Gaya",
+    url: "./songs/Punjabi Tadka/p12.mp3",
+  },
+  {
+    songName: "Jutti Jharh Ke",
+    url: "./songs/Punjabi Tadka/p13.mp3",
+  },
+  {
+    songName: "Lifelines",
+    url: "./songs/Punjabi Tadka/p14.mp3",
+  },
+  {
+    songName: "Nawa Daur",
+    url: "./songs/Punjabi Tadka/p15.mp3",
+  },
+  {
+    songName: "Phone Cover",
+    url: "./songs/Punjabi Tadka/p16.mp3",
+  },
+  {
+    songName: "Snap",
+    url: "./songs/Punjabi Tadka/p17.mp3",
+  },
+  {
+    songName: "Tareyan Di Loh",
+    url: "./songs/Punjabi Tadka/p18.mp3",
+  },
+  {
+    songName: "Tu Juliet Jatt Di",
+    url: "./songs/Punjabi Tadka/p19.mp3",
+  },
+  {
+    songName: "U Dnt Even Knw",
+    url: "./songs/Punjabi Tadka/p20.mp3",
+  },
+  {
+    songName: "Wooden Baby",
+    url: "./songs/Punjabi Tadka/p21.mp3",
+  },
+  {
+    songName: "Yaadan De Dhage",
+    url: "./songs/Punjabi Tadka/p21.mp3",
+  },
+];
+var arr2 = [
+  {
+    songName: "At My Wrost",
+    url: "./songs/With Him Tadka/w1.mp3",
+  },
+  {
+    songName: "Can We Kiss Forever",
+    url: "./songs/With Him Tadka/w2.mp3",
+  },
+  {
+    songName: "Can't Lie",
+    url: "./songs/With Him Tadka/w3.mp3",
+  },
+  {
+    songName: "Dancing With Your",
+    url: "./songs/With Him Tadka/w4.mp3",
+  },
+  {
+    songName: "Falling",
+    url: "./songs/With Him Tadka/w5.mp3",
+  },
+  {
+    songName: "Favorite",
+    url: "./songs/With Him Tadka/w6.mp3",
+  },
+  {
+    songName: "Get You The Moon",
+    url: "./songs/With Him Tadka/w7.mp3",
+  },
+  {
+    songName: "Hold On",
+    url: "./songs/With Him Tadka/w8.mp3",
+  },
+  {
+    songName: "I Like The Way You Kiss Me",
+    url: "./songs/With Him Tadka/w9.mp3",
+  },
+  {
+    songName: "I Wanna Be Yours",
+    url: "./songs/With Him Tadka/w10.mp3",
+  },
+  {
+    songName: "If The World Was End",
+    url: "./songs/With Him Tadka/w11.mp3",
+  },
+  {
+    songName: "If We Have Each Other",
+    url: "./songs/With Him Tadka/w12.mp3",
+  },
+  {
+    songName: "I'm Yours Speed Up",
+    url: "./songs/With Him Tadka/w13.mp3",
+  },
+  {
+    songName: "It's You",
+    url: "./songs/With Him Tadka/w14.mp3",
+  },
+  {
+    songName: "Mine",
+    url: "./songs/With Him Tadka/w15.mp3",
+  },
+  {
+    songName: "Myself",
+    url: "./songs/With Him Tadka/w16.mp3",
+    img: "./images/animal.jpg",
+  },
+  {
+    songName: "Perfect",
+    url: "./songs/With Him Tadka/w17.mp3",
+  },
+  {
+    songName: "Sa Yes To Heaven",
+    url: "./songs/With Him Tadka/w18.mp3",
+  },
+  {
+    songName: "Stereo Hearts",
+    url: "./songs/With Him Tadka/w19.mp3",
+  },
+  {
+    songName: "What If I Told you",
+    url: "./songs/With Him Tadka/w20.mp3",
+  },
+];
+var arr3 = [
+  {
+    songName: "Arjan Vailly Ne",
+    url: "./songs/Random Tadka/Arjan Vailly Ne.mp3",
+  },
+  {
+    songName: "Dekha Tenu",
+    url: "./songs/Random Tadka/Dekha Tenu Pehli Pehli Baar Ve.mp3",
+  },
+  {
+    songName: "God Damn",
+    url: "./songs/Random Tadka/God Damn.mp3",
+  },
+  {
+    songName: "Gulabi Sadi Ani Lali",
+    url: "./songs/Random Tadka/Gulabi Sadi Ani Lali.mp3",
+  },
+  {
+    songName: "G Yaan",
+    url: "./songs/Random Tadka/gyaan.mp3",
+  },
+  {
+    songName: "Heeriye",
+    url: "./songs/Random Tadka/heeriye.mp3",
+  },
+  {
+    songName: "Jale 2",
+    url: "./songs/Random Tadka/Jale 2.mp3",
+  },
+  {
+    songName: "O Sajni Re",
+    url: "./songs/Random Tadka/O Sajni Re.mp3",
+  },
+  {
+    songName: "Pehle Bhi Main",
+    url: "./songs/Random Tadka/Pehle Bhi Main.mp3",
+  },
+  {
+    songName: "Ram Siya Ram",
+    url: "./songs/Random Tadka/Ram Siya Ram.mp3",
+  },
+  {
+    songName: "Soulmate",
+    url: "./songs/Random Tadka/soulmate.mp3",
+  },
+  {
+    songName: "Tu Hai Kahan",
+    url: "./songs/Random Tadka/Tu Hai Kahan.mp3",
+  },
+  {
+    songName: "Yimmy Yimmy",
+    url: "./songs/Random Tadka/Yimmy Yimmy.mp3",
+  },
+];
+
+var currentPlaylist = arr0;
+var selectedSong = 0;
+var audio = new Audio(currentPlaylist[selectedSong].url);
+var active = 0;
+var playing = false;
+
+function mainFunction() {
+  if (active == 0) {
+    updatePlaylistUI(currentPlaylist);
+  }
+  active++;
+}
+document.querySelector("#songheading").innerHTML =
+  currentPlaylist[selectedSong].songName;
+function updatePlaylistUI(playlist) {
+  var clutter = "";
+  playlist.forEach(function (elem, index) {
+    clutter += `<div class="card-library">
+      <div class="card-left"><img src="images/music.svg" alt="" />
+      <div class="info-library">
+        <h4>${elem.songName}</h4>
+        <p>Ashutosh</p>
+      </div></div>
+      <div data-index=${index} class="play-library">
+        <img id="card-play${index}" data-index=${index} class="pointer" src="images/play.svg" alt="" />
+      </div>
+    </div>`;
+  });
+  document.querySelector(".library-card-container").innerHTML = clutter;
+}
+
+mainFunction();
+
+document
+  .querySelector(".library-card-container")
+  .addEventListener("click", function (detail) {
+    if (detail.target.dataset.index !== undefined) {
+      let clickedIndex = parseInt(detail.target.dataset.index, 10);
+      if (clickedIndex === selectedSong) {
+        if (playing) {
+          playing = false;
+          audio.pause();
+          detail.target.src = "images/play.svg";
+          document.querySelector("#play").src = "images/play.svg";
+        } else {
+          playing = true;
+          audio.play();
+          detail.target.src = "images/pause.svg";
+          document.querySelector("#play").src = "images/pause.svg";
+        }
+      } else {
+        selectedSong = clickedIndex;
+        updateSong();
+        playing = true;
+        audio.play();
+        detail.target.src = "images/pause.svg";
+        document.querySelector("#play").src = "images/pause.svg";
+      }
+    }
+  });
+
+function updatePlaylist(newPlaylist) {
+  currentPlaylist = newPlaylist;
+  selectedSong = 0;
+
+  updatePlaylistUI(newPlaylist);
+  updateSong();
+}
+
+function updateSong() {
+  document.querySelector("#songheading").innerHTML =
+    currentPlaylist[selectedSong].songName;
+  audio.src = currentPlaylist[selectedSong].url;
+  resetLibraryPlayButtons();
+  if (playing) {
+    audio.play();
+    document.querySelector(`#card-play${selectedSong}`).src =
+      "images/pause.svg";
+    document.querySelector("#play").src = "images/pause.svg";
+  } else {
+    audio.pause();
+    document.querySelector(`#card-play${selectedSong}`).src = "images/play.svg";
+    document.querySelector("#play").src = "images/play.svg";
+  }
+}
+function resetLibraryPlayButtons() {
+  currentPlaylist.forEach((_, index) => {
+    document.querySelector(`#card-play${index}`).src = "images/play.svg";
+  });
+}
+document
+  .querySelector(".album-container")
+  .addEventListener("click", function (detail) {
+    if (detail.target.dataset.index !== undefined) {
+      let playlistIndex = detail.target.dataset.index;
+      if (playlistIndex == 0) {
+        updatePlaylist(arr0);
+      } else if (playlistIndex == 1) {
+        updatePlaylist(arr1);
+      } else if (playlistIndex == 2) {
+        updatePlaylist(arr2);
+      } else if (playlistIndex == 3) {
+        updatePlaylist(arr3);
+      }
+    }
+  });
+
+document.querySelector(".seekbar").addEventListener("click", (e) => {
+  let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+  document.querySelector(".circle").style.left = percent + "%";
+  audio.currentTime = (audio.duration * percent) / 100;
+});
+
+var backward = document.querySelector("#back");
+var play = document.querySelector("#play");
+var forward = document.querySelector("#forward");
+
+forward.addEventListener("click", function () {
+  if (selectedSong < currentPlaylist.length - 1) {
+    selectedSong++;
+    updateSong();
+    backward.style.opacity = "1";
+  } else {
+    forward.style.opacity = "0.2";
+  }
+});
+
+backward.addEventListener("click", function () {
+  if (selectedSong > 0) {
+    selectedSong--;
+    updateSong();
+    forward.style.opacity = "1";
+  } else {
+    backward.style.opacity = "0.2";
+  }
+});
+
+play.addEventListener("click", function () {
+  playing = !playing;
+  if (playing) {
+    play.src = "images/pause.svg";
+    document.querySelector(`#card-play${selectedSong}`).src =
+      "images/pause.svg";
+    audio.play();
+  } else {
+    play.src = "images/play.svg";
+    document.querySelector(`#card-play${selectedSong}`).src = "images/play.svg";
+    audio.pause();
+  }
+});
+
+audio.addEventListener("timeupdate", () => {
+  document.querySelector(".startTime").innerHTML = `${secondsToMinutesSeconds(
+    audio.currentTime
+  )}`;
+  document.querySelector(".endTime").innerHTML = `${secondsToMinutesSeconds(
+    audio.duration
+  )}`;
+  document.querySelector(".circle").style.left =
+    (audio.currentTime / audio.duration) * 98 + "%";
+});
+
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+document.querySelector(".range").addEventListener("input", (e) => {
+  audio.volume = parseInt(e.target.value) / 100;
+  if (audio.volume > 0) {
+    document.querySelector(".volume>img").src = "images/volume.svg";
+  }
+});
+
+let previousVolume = audio.volume;
+
+document.querySelector(".volume>img").addEventListener("click", (e) => {
+  if (audio.volume > 0) {
+    previousVolume = audio.volume;
+    e.target.src = "images/mute.svg";
+    audio.volume = 0;
+    document.querySelector(".range").value = 0;
+  } else {
+    e.target.src = "images/volume.svg";
+    audio.volume = previousVolume;
+    document.querySelector(".range").value = previousVolume * 100;
+  }
+});
+
+document.querySelector("#hamburger").addEventListener("click", () => {
+  document.querySelector(".left").style.left = "0%";
+});
+
+document.querySelector("#close").addEventListener("click", () => {
+  document.querySelector(".left").style.left = "-100%";
+});
